406cc668208edbd0cd6455dcd7b46bf97bf57972
Jessé Bezerra <jessebezerra@hotmail.com.br>
2023-02-25 12:50:48 -0300

    .

--- app/src/main/java/br/com/bs/app/controller/ZMessageController.java
+++ app/src/main/java/br/com/bs/app/controller/ZMessageController.java
@@ -2,6 +2,9 @@ package br.com.bs.app.controller;
 
 import br.com.bs.app.payload.ZMessageRequest;
 import br.com.bs.app.service.ZMessageService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,21 +12,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("api/z-watts")
 public class ZMessageController {
 
     @Autowired
     private ZMessageService service;
-
-    @PostMapping("teste")
-    public void teste(){
-        System.out.println("teste");
-    }
-
-
+    @ApiOperation("Envia uma mensagem para o telefone informado")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+    })
     @PostMapping("message")
-    public ResponseEntity sendMessage(@RequestBody ZMessageRequest request) {
+    public ResponseEntity sendMessage(@Valid @RequestBody ZMessageRequest request) {
         return ResponseEntity.ok().body(service.sendMessage(request));
     }
 }
